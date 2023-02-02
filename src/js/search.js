a5cba7305c145b9bbe005eef4391f9bfd0a2b4c3
Alexey Gorchakov <amvrogor@gmail.com>
2023-02-02 18:49:25 +0200

    1

--- src/js/search.js
+++ src/js/search.js
@@ -1,1 +1,22 @@
-import axios from 'axios';
+export { onSearch };
+import { getSearchMovieAPI } from './show-results';
+import Notiflix from 'notiflix';
+
+function onSearch(e) {
+  e.preventDefault();
+  query = e.currentTarget.elements[0].value.trim().toLowerCase();
+  let page = 1;
+
+  getSearchMovieAPI(query, page)
+    .then(data => {
+      if (data.results.length === 0) {
+        Notiflix.Notify.failure(
+          'Search result not successful. Enter the correct movie name and retry.'
+        );
+      } else {
+        // console.log(data.results);
+        gallery.innerHTML = createMarkup(data);
+      }
+    })
+    .catch(err => console.log(err));
+}
