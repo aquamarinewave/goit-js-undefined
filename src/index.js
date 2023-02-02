d98b31052dbdc04684892f3839d1ad1d46608999
Maksym Stankov <maksym.stankov@gmail.com>
2023-02-02 13:12:43 +0200

    Додовання API
    1. Додав GLOBAL_ KEY
    2.Додав функцію отримання трендових фільмів
    3. Додав функцію отримання фільму на запитом
    4.Додав функцію отримання інформації про фільм за Id

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import getApi from './js/show-results';
+import {
+  getTrendingAPI,
+  getSearchMovieAPI,
+  getMovieInformationForIdAPI,
+} from './js/show-results';
+
+getTrendingAPI().then(data => console.log(data));
+getSearchMovieAPI('cat').then(data => console.log(data));
+getMovieInformationForIdAPI(454646).then(data => console.log(data));
