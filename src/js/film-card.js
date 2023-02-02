0f31be9d9df4c5844ed64aaed761171980ebd8db
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-02-02 19:55:03 +0200

    add film card

--- src/js/film-card.js
+++ src/js/film-card.js
@@ -1,13 +1,10 @@
-<div class="card-container">
-    <img src="./images/film-img.jpg" class="card-pic">
-    <div class="information">
+export const filmCard = `<div class="card-container">
+    <img  class="card-pic" src="./images/film-img.jpg">
     <p class="film-name">ONCE UPON A TIME... IN HOLLYWOOD</p>
     <div class="additional-information">
         <ul class="gener-list">
             <li class="gener-item">Drama, Comedy</li>
         </ul>
-        <div class="vl"></div>
         <p class="release-year">2019</p>
     </div>
-    </div>
-</div>
+</div>`;
