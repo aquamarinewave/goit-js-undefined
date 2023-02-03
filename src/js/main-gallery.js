ebfa25740dee988bec33400f5a70895dcec9ebc8
Anton Kulakov <92113717+AntonK33@users.noreply.github.com>
2023-02-03 21:03:01 +0200

    розмітка головної сторінки

---
+++ src/js/main-gallery.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { getTrendingAPI } from './show-results';
+const mainGallery = document.querySelector(".main-gallery");
+
+//function mainPageTrendingMovie() {
+//    const GLOBAL_KEY = 'df88ba4f44a5ed712dd0a71f1b3d877c';
+ //   const APItrending = `https://api.themoviedb.org/3/trending/movie/week?api_key=5d369441bd67a61471936fd86df9d048`;
+
+//    return axios.get(APItrending);
+//rederMainPage(resalts)
+//}
+console.log(getTrendingAPI)
+ function rederMainPage(data) {
+  const imageURL = `https://image.tmdb.org/t/p/w500`;
+   let markup = data.results.map(({ poster_path, title }) =>
+     `
+     <li class="movie-card">
+     <a  href=''><img src=${imageURL}${poster_path} alt=${title} loading="lazy" height=574px width=395px /></a>
+    <p class="info-item">
+      <b> ${title}</b>
+    </p>
+    
+     </li>
+    
+    `
+   ).join("");
+    
+  mainGallery.innerHTML = markup;
+   
+}
+
+async function fetchHandler() {
+  getTrendingAPI()
+.then(results=>rederMainPage(results))
+}
+
+fetchHandler()
