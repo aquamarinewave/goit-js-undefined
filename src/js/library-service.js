8661e81e7b291e61f93b4414f417ffa07a80c5ec
zhdima <zhdima@gmail.com>
2023-02-05 13:34:28 +0200

    library-in-modal

--- src/js/library-service.js
+++ src/js/library-service.js
@@ -15,11 +15,15 @@ for (const key of ['watched', 'queue']) {
   myLib.clearMovies();
   for (let i = 0; i < 20; i += 1) {
     myLib.addMovie({
-      id: i + 1,
+      id: String(i + 1),
       title: `Avatar: The Way of Water (${key})`,
       posterURL: 'https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg',
       genres: 'Drama, Comedy',
-      year: 2022,
+      year: '2022',
+      vote: '5.7',
+      votes: '1234',
+      popularity: '100.2',
+      original: `Avatar: The Way of Water`,
       overview: `Set more than a decade after the events of the first film, learn the story of the Sully family 
         (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe,
         the battles they fight to stay alive, and the tragedies they endure.`
