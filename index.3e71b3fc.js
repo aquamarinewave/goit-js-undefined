d6da75a75a3192042b80a8f5416f5f82279d8f82
aquamarinewave <50908486+aquamarinewave@users.noreply.github.com>
2023-02-06 16:23:42 +0000

    Deploying to gh-pages from @ aquamarinewave/goit-js-undefined@a0786507aaa6d005337bc1705fec0a9f1fe86d8c 🚀

--- index.3e71b3fc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".modal__button-cls"),o=document.querySelector(".overlay");console.log(o);const c=document.querySelector(".js-modal-container");console.log(c),e.addEventListener("click",(function(){o.classList.add("visually-hidden")})),c.addEventListener("click",(function(){o.classList.remove("visually-hidden")}));
-//# sourceMappingURL=index.3e71b3fc.js.map
--- index.b761bfed.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".modal__button-cls"),o=document.querySelector(".overlay");console.log(o);const n=document.querySelector(".js-modal-container");console.log(n),e.addEventListener("click",(function(){o.classList.add("visually-hidden")})),n.addEventListener("click",(function(){o.classList.remove("visually-hidden")}))}();
-//# sourceMappingURL=index.b761bfed.js.map
