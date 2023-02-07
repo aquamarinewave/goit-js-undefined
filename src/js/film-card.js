ffd421a9dfeeda208e49a2bda0ca9eaa897d3959
Maryna Medynska <maryna.medynska@gmail.com>
2023-02-08 01:56:50 +0200

    Corect film card

--- src/js/film-card.js
+++ src/js/film-card.js
@@ -3,7 +3,7 @@ import { noPosterURL } from './library-modal';
 export default
 function createFilmCardMarkup({id, posterURL = '', title = '', genres = '', year = '', vote = '' }) {
   return `<li class="card" data-filmid="${id}">
-        <img src="${posterURL?posterURL:noPosterURL}" alt="${title}" class="card__pic">
+        <div class="card__wrapper"><img src="${posterURL?posterURL:noPosterURL}" alt="${title}" class="card__pic"></div>
         <div class="card__information">
             <p class="card__film-name">${title}</p>
             <div class="card__additional-information">
